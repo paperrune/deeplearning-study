67b10c9dc03e9708723c1f2613fbbec7079f9c2d
paperrune <paperrune@naver.com>
2017-10-09 02:51:20 +0900

    Update main.cpp

--- Recurrent_Neural_Networks/main.cpp
+++ Recurrent_Neural_Networks/main.cpp
@@ -90,17 +90,17 @@ void Read_MNIST(char training_set_images[], char training_set_labels[], char tes
 int main(){
 	char *type_layer[] = {"MNIST", "Clstm", "Lce,sm"};
 
-	int batch_size		 = 60;
-	int time_step		 = 28;
-	int time_stride		 = 28;
-	int number_map[]	 = {784 / time_step, 100, 10};
-	int number_iteration = 100;
-	int number_layer	 = sizeof(type_layer) / sizeof(type_layer[0]);
-	int number_thread	 = 6;
-	int number_training	 = 6000;
-	int number_test		 = 1000;
+	int batch_size			= 60;
+	int time_step			= 28;
+	int time_stride			= 28;
+	int number_maps[]		= {784 / time_step, 100, 10};
+	int number_iterations	= 100;
+	int number_layers		= sizeof(type_layer) / sizeof(type_layer[0]);
+	int number_threads		= 6;
+	int number_training		= 6000;
+	int number_test			= 1000;
 
-	/* Training using the entire dataset takes about 800 seconds per iteration on the i7-4790K with 6 threads.
+	/* Training using the entire dataset takes about 1000 seconds per iteration on the i7-4790K with 6 threads.
 	int number_training	 = 60000;
 	int number_test		 = 10000;
 	*/
@@ -115,7 +115,7 @@ int main(){
 	double ***input			= new double**[number_training + number_test];
 	double ***target_output	= new double**[number_training + number_test];
 
-	Recurrent_Neural_Networks *RNN = new Recurrent_Neural_Networks(type_layer, number_layer, 0, number_map);
+	Recurrent_Neural_Networks *RNN = new Recurrent_Neural_Networks(type_layer, number_layers, 0, number_maps);
 
 	for(int h = 0;h < number_training + number_test;h++){
 		length_data[h] = time_step;
@@ -124,8 +124,8 @@ int main(){
 		target_output[h] = new double*[length_data[h]];
 
 		for(int t = 0;t < length_data[h];t++){
-			input[h][t]			= new double[number_map[0]];
-			target_output[h][t] = new double[number_map[number_layer - 1]];
+			input[h][t]			= new double[number_maps[0]];
+			target_output[h][t] = new double[number_maps[number_layers - 1]];
 		}
 	}
 	for(int t = 0;t < time_step;t++){
@@ -134,14 +134,14 @@ int main(){
 	Read_MNIST("train-images.idx3-ubyte", "train-labels.idx1-ubyte", "t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte", time_step, number_training, number_test, input, target_output);	
 
 	RNN->Initialize_Parameter(0, 0.2, -0.1);
-	omp_set_num_threads(number_thread);
+	omp_set_num_threads(number_threads);
 
-	for(int h = 0, time = clock();h < number_iteration;h++){
+	for(int h = 0, time = clock();h < number_iterations;h++){
 		int number_correct[2] = {0, };
 
 		double loss = RNN->Train(batch_size, number_training, time_step, time_stride, length_data, output_mask, epsilon, gradient_threshold, learning_rate, input, target_output);
 
-		double *output = new double[number_map[number_layer - 1]];
+		double *output = new double[number_maps[number_layers - 1]];
 
 		for(int i = 0;i < number_training + number_test;i++){
 			for(int t = 0;t < time_step;t++){
@@ -152,7 +152,7 @@ int main(){
 				RNN->Test(t == 0, input[i][t], output);
 
 				if(output_mask == 0 || output_mask[t]){
-					for(int j = 0;j < number_map[number_layer - 1];j++){
+					for(int j = 0;j < number_maps[number_layers - 1];j++){
 						if(max < output[j]){
 							argmax = j;
 							max = output[j];
