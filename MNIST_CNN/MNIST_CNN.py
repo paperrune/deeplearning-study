fb9d324744f52f082fd16b4986a4198c34d780c5
paperrune <paperrune@naver.com>
2018-05-13 02:07:41 +0900

    Update MNIST_CNN.py

--- MNIST_CNN/MNIST_CNN.py
+++ MNIST_CNN/MNIST_CNN.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-from Neural_Networks import *
+from Neural_Networks import NNCPU
 
 def Read_MNIST(training_set_images, training_set_labels, test_set_images, test_set_labels, number_training, number_test, image, target_output):
     # training set images
@@ -65,7 +65,7 @@ number_training = 60000
 number_test     = 10000
 
 # train from scratch
-NN = Neural_Networks()
+NN = NNCPU.Neural_Networks()
 
 NN.Add_Layer("MNIST", 1, 28, 28)
 NN.Add_Layer("BN,ReLU", 24, 24, 24) # batch normalization, ReLU activation
