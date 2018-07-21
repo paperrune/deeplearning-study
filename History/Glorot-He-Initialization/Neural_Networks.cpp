3fe2df4dbdf3155f3acfd5debbb15d7fc591b3e2
paperrune <paperrune@naver.com>
2018-07-21 14:12:07 +0900

    Update Neural_Networks.cpp

--- History/Glorot-He-Initialization/Neural_Networks.cpp
+++ History/Glorot-He-Initialization/Neural_Networks.cpp
@@ -947,7 +947,7 @@ double Neural_Networks::Fit(float **x_train, float **y_train, int train_size, in
 		if (++h == batch_size || g == train_size - 1) {
 			Resize_Memory(h);
 
-			// prepare dropout mask if specified
+			// prepare dropout_mask if dropout specified
 			for (int i = 1; i < layer.size(); i++) {
 				Layer *layer = this->layer[i];
 
