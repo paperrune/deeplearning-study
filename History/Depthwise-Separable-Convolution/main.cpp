d53d530fb9d3e71964fb9fad2d243a329e1f2b64
paperrune <paperrune@naver.com>
2018-08-09 06:10:52 +0900

    Update main.cpp

--- History/Depthwise-Separable-Convolution/main.cpp
+++ History/Depthwise-Separable-Convolution/main.cpp
@@ -122,9 +122,9 @@ int main() {
 		y_data[h] = new float[number_nodes[1]];
 	}
 	Read_MNIST(path + "train-images.idx3-ubyte", path + "train-labels.idx1-ubyte", path + "t10k-images.idx3-ubyte", path + "t10k-labels.idx1-ubyte", number_training, number_test, x_data, y_data);
-	omp_set_num_threads(number_threads);
 
 	srand(0);
+	omp_set_num_threads(number_threads);
 
 	NN.Add( 1, 28, 28);
 	NN.Add(24, 24, 24)->Activation(Activation::relu)->Batch_Normalization();
@@ -143,7 +143,7 @@ int main() {
 	NN.Connect(6, 5, "W")->Initializer(HeNormal());
 	NN.Connect(7, 6, "W")->Initializer(HeNormal());
 
-	NN.Compile(Loss::cross_entropy, new Optimizer(SGD(learning_rate)));
+	NN.Compile(Loss::cross_entropy, Optimizer(SGD(learning_rate)));
 
 	for (int e = 0, time = clock(); e < epochs; e++) {
 		int score[2] = { 0, };
@@ -177,10 +177,6 @@ int main() {
 		}
 		printf("loss: %.4f / %.4f	accuracy: %.4f / %.4f	step %d  %.2f sec\n", loss[0], loss[1], 1.0 * score[0] / number_training, 1.0 * score[1] / number_test, e + 1, (double)(clock() - time) / CLOCKS_PER_SEC);
 
-		for (int h = 0; h < batch_size; h++) {
-			delete[] output[h];
-		}
-		delete[] _input;
 		delete[] output;
 	}
 
