e18233cfc815499a61fee554edc0a54718a44bb9
paperrune <paperrune@naver.com>
2019-01-28 19:32:39 +0900

    Update Keras.py

--- Dropout/Keras.py
+++ Dropout/Keras.py
@@ -12,10 +12,10 @@ num_classes = 10
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784).astype('float32') / 255
+x_train = x_train.reshape([x_train.shape[0], -1]).astype('float32') / 255
 y_train = to_categorical(y_train, num_classes)
 
-x_test = x_test.reshape(10000, 784).astype('float32') / 255
+x_test = x_test.reshape([x_test.shape[0], -1]).astype('float32') / 255
 y_test = to_categorical(y_test, num_classes)
 
 model = Sequential()
