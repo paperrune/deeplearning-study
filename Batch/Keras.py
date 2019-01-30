b7d1709655ecb00817633e05538b8a87e9071653
paperrune <paperrune@naver.com>
2019-01-30 13:58:51 +0900

    Update Keras.py

--- Batch/Keras.py
+++ Batch/Keras.py
@@ -8,18 +8,17 @@ from keras.utils import to_categorical
 batch_size = 128
 epochs = 20
 learning_rate = 0.5
-num_classes = 10
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape([x_train.shape[0], -1]).astype('float32') / 255
-y_train = to_categorical(y_train, num_classes)
+y_train = to_categorical(y_train, num_classes=10)
 
 x_test = x_test.reshape([x_test.shape[0], -1]).astype('float32') / 255
-y_test = to_categorical(y_test, num_classes)
+y_test = to_categorical(y_test, num_classes=10)
 
 model = Sequential()
-model.add(Dense(num_classes,
+model.add(Dense(10,
                 activation='sigmoid',
                 input_shape=(784,),
                 kernel_initializer=RandomUniform(minval=-0.01, maxval=0.01)))
