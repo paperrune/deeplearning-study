afa207abfb7cbc2a0085bb0c7a9cf58a165bed66
paperrune <paperrune@naver.com>
2019-02-08 20:14:40 +0900

    Update Keras.py

--- Perceptron/Keras.py
+++ Perceptron/Keras.py
@@ -27,8 +27,8 @@ model.compile(loss='mean_squared_error',
               optimizer=SGD(lr=learning_rate),
               metrics=['accuracy'])
 
-history = model.fit(x_train,
-                    y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    validation_data=(x_test, y_test))
+model.fit(x_train,
+          y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
