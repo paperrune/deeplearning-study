a810b47679d0caa3615e7baa51e20780bb9c9ac4
paperrune <paperrune@naver.com>
2019-02-08 20:16:07 +0900

    Update Keras.py

--- ReLU/Keras.py
+++ ReLU/Keras.py
@@ -33,8 +33,8 @@ model.compile(loss='categorical_crossentropy',
               optimizer=SGD(lr=learning_rate),
               metrics=['accuracy'])
 
-history = model.fit(x_train,
-                    y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    validation_data=(x_test, y_test))
+model.fit(x_train,
+          y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
