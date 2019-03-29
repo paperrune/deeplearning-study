5b7106a98bd59e07f827212574207f3a6d9bf28c
paperrune <paperrune@naver.com>
2019-03-29 14:30:34 +0900

    Update Tensorflow.py

--- Variational-Autoencoder/Tensorflow.py
+++ Variational-Autoencoder/Tensorflow.py
@@ -111,10 +111,10 @@ decoder = L5
 hypothesis = tf.nn.sigmoid(tf.matmul(tf.nn.relu(tf.matmul(z, W4) + b4), W5) + b5)
 
 reconstruction_loss = (hypothesis - X) * (hypothesis - X) * input_dim
-reguralization_loss = 0.5 * tf.reduce_sum(tf.exp(z_log_var) + tf.square(z_mean) - z_log_var - 1, axis=-1, keepdims=True)
+regularization_loss = 0.5 * tf.reduce_sum(tf.exp(z_log_var) + tf.square(z_mean) - z_log_var - 1, axis=-1, keepdims=True)
 
 # cost & optimizer
-cost = tf.reduce_mean(reguralization_loss + reconstruction_loss)
+cost = tf.reduce_mean(regularization_loss + reconstruction_loss)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train = optimizer.minimize(cost)
 
